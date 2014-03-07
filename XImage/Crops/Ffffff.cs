31afc0c914d7e5db218c6dc16fc6c2ff2eba9c38
Rylan Barnes <rylan.barnes@gmail.com>
2014-03-07 05:34:36 -0600

    Provides backwards compatibility where you could specify the color arg without a function name.

---
+++ XImage/Crops/Ffffff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace XImage.Crops
+{
+	/// <summary>
+	/// Provides backwards compatibility where you could specify the color arg without a function name.
+	/// </summary>
+	public class Ffffff : Fit
+	{
+		public Ffffff()
+		{
+			Color = Color.White;
+		}
+	}
+}
