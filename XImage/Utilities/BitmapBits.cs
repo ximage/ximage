c5df3d25e4372d3d9d02df33c4513f29b82ed19e
Rylan Barnes <rylan.barnes@gmail.com>
2014-03-13 14:50:23 -0500

    Lazy copies the image buffer into managed code.

--- XImage/Utilities/BitmapBits.cs
+++ XImage/Utilities/BitmapBits.cs
@@ -8,12 +8,28 @@ using System.Web;
 
 namespace XImage.Utilities
 {
+	/// <summary>
+	/// Exposes a BitmapData and a managed, lazy copied, byte array to work on.  
+	/// Optionally will auto-copy modified bytes back to the image on dispose.
+	/// </summary>
 	public class BitmapBits : IDisposable
 	{
 		Bitmap _bitmap;
 		bool _writeAccess;
 
-		public byte[] Data { get; private set; }
+		byte[] _data = null;
+		public byte[] Data
+		{
+			get
+			{
+				if (_data == null)
+				{
+					_data = new byte[BitmapData.Stride * _bitmap.Height];
+					Marshal.Copy(BitmapData.Scan0, _data, 0, Data.Length);
+				}
+				return _data;
+			}
+		}
 		public BitmapData BitmapData { get; private set; }
 
 		public BitmapBits(Bitmap bitmap, bool writeAccess = false)
@@ -23,23 +39,22 @@ namespace XImage.Utilities
 			BitmapData = _bitmap.LockBits(
 				rect: new Rectangle(Point.Empty, _bitmap.Size), 
 				flags: _writeAccess ? ImageLockMode.ReadWrite : ImageLockMode.ReadOnly,
-				format:_bitmap.PixelFormat);
-			Data = new byte[BitmapData.Stride * _bitmap.Height];
-			Marshal.Copy(BitmapData.Scan0, Data, 0, Data.Length);
+				format: _bitmap.PixelFormat);
 		}
 
 		public void Dispose()
 		{
 			if (BitmapData != null && _bitmap != null)
 			{
-				if (_writeAccess)
+				// Copies the data back.
+				if (_writeAccess && _data != null)
 					Marshal.Copy(Data, 0, BitmapData.Scan0, Data.Length);
 
 				_bitmap.UnlockBits(BitmapData);
 			}
 			BitmapData = null;
 			_bitmap = null;
-			Data = null;
+			_data = null;
 		}
 	}
 }
